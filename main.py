fcbe7627018e1945cb0a4dca0da869ea7d72d730
Malloy Okon <mokut.okon4@gmail.com>
2022-02-17 12:37:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import string
+
+from collections import Counter 
+
+from bs4 import BeautifulSoup
+
+url = "https://www.morningbrew.com/daily"
+
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+links = soup.findAll("a")
+
+news_urls = []
+for link in links:
+    href = link.get("href")
+    if href.startswith("/daily"):
+        news_url = "https://www.morningbrew.com" + href
+        news_urls.append(news_url)
+
+
+all_nouns = []
+for url in news_urls[:10]:
+    print("Fetching {}".format(url))
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    words = soup.text.split()
+    nouns = [word for word in words if word.isalpha() and word[0] in string.ascii_uppercase]
+    all_nouns += nouns
+
+print(Counter(all_nouns).most_common(100))
+
+
